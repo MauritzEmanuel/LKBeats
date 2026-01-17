284f72a131de2b4b8fa4ebce4b4c16efdf3486fb
MauritzEmanuel <emanuel.lundbergh@yh.nackademin.se>
2026-01-17 18:47:49 +0100

    comment

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,6 +1,6 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+/*import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'*/
 
 
 //Not used anymore, kept for possible future use.
